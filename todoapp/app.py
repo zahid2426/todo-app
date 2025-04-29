75b971f2ba9cf38f5be1e68daa7bd27492902f5f
Zahid Zubair <zahidzubair03@gmail.com>
2025-04-30 00:55:07 +0530

    newbranch

--- todoapp/app.py
+++ todoapp/app.py
@@ -61,16 +61,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create DB table if not exists
     app.run(debug=True, host='0.0.0.0', port=5000)
-
-from flask import Flask, render_template
-
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
-
-
